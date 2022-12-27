805a779faa9aa066c5f7678f92b3baa3946c86a9
Mohammed Saqlain <120318579+Sq1754@users.noreply.github.com>
2022-12-27 20:21:28 +0530

    Update Q5.java

--- Q5.java
+++ Q5.java
@@ -7,6 +7,13 @@ public static void main(String[]args) {
 	Scanner sc = new Scanner(System.in);
 	
 	//Q5 : Print floyed's triangle ?
+	/*
+	   1
+	   2 3
+	   4 5 6
+	   7 8 9 10
+	   
+	*/
 	
 	System.out.println("Give me a number");
 	int rows=sc.nextInt();
